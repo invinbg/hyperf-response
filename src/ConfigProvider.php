c5f2b00fbd2fc0a95048272200c322675b58b23c
蔡嘉俊 <caijiajun@caijiajundeMacBook-Pro.local>
2020-07-23 15:46:01 +0800

    fix bug

--- src/ConfigProvider.php
+++ src/ConfigProvider.php
@@ -1,6 +1,17 @@
 <?php
-
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
 namespace Invinbg\Hyperf\Response;
+
+use Invinbg\Hyperf\Response\Contract\ResponseInterface;
+
 class ConfigProvider
 {
     public function __invoke(): array
